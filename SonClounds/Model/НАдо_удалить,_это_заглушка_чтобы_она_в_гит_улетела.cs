6633ab321d162e2ae5a37c6531fa20095c370ec1
ivansls <saldaykin1@gmail.com>
2023-06-10 20:22:27 +0300

    MVVm

---
+++ SonClounds/Model/НАдо_удалить,_это_заглушка_чтобы_она_в_гит_улетела.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SonClounds.Model
+{
+    internal class НАдо_удалить__это_заглушка_чтобы_она_в_гит_улетела
+    {
+    }
+}
