82ef5cce7b3390204384303bc48b159b61cc3544
ivansls <saldaykin1@gmail.com>
2023-06-10 20:08:24 +0300

    MVVM

---
+++ SonClounds/Model/ЗАГЛУШКА_ЕЁ_надо_удалить.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SonClounds.Model
+{
+    class ЗАГЛУШКА_ЕЁ_надо_удалить
+    {
+    }
+}
