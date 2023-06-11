b6cb424ebc66907dd97cb5b9be932ad86fb05a80
AlexqPr <isip_a.v.mic@mpt.ru>
2023-06-11 21:04:19 +0300

    Первая страница Демо.dmg

--- SonClounds/View/MainWindow.xaml.cs
+++ SonClounds/View/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SonClounds.View;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,7 @@ namespace SonClounds
         public MainWindow()
         {
             InitializeComponent();
+            PageFrame.Content = new First();
         }
     }
 }
