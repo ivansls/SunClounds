c1bc83262f187c4bd1864822573dfe2e1f8cd6fc
anastasiaoreskina <anastasiaoreskina@CCF1>
2023-06-12 02:01:21 +0300

    Started working on the first window

--- SonClounds/View/StartWindow.xaml.cs
+++ SonClounds/View/StartWindow.xaml.cs
@@ -10,17 +10,16 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace SonClounds
+namespace SonClounds.View
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Логика взаимодействия для StartWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class StartWindow : Window
     {
-        public MainWindow()
+        public StartWindow()
         {
             InitializeComponent();
         }
