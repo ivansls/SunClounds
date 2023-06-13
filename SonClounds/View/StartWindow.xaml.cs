2d4862bab1ca329b02d3e8f7af1d4f7e4c89a846
anastasiaoreskina <anastasiaoreskina@CCF1>
2023-06-13 10:31:19 +0300

    Proper shadow is done

--- SonClounds/View/StartWindow.xaml.cs
+++ SonClounds/View/StartWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SonClounds.ViewModel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -22,6 +23,7 @@ namespace SonClounds.View
         public StartWindow()
         {
             InitializeComponent();
+            DataContext = new StartWindowViewModel();
         }
     }
 }
---
+++ SonClounds/ViewModel/StartWindowViewModel.cs
@@ -0,0 +1,29 @@
+﻿using pr7.ViewModel.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SonClounds.ViewModel
+{
+    internal class StartWindowViewModel:BindingHelper
+    {
+
+        #region Commands
+        public BindableCommand CloseCommand { get; set; }
+        public BindableCommand LessCommand { get; set; }
+        public BindableCommand AllScreenCommand { get; set; }
+        public BindableCommand TextStartedChangingCommand { get; set; }
+        #endregion
+        public StartWindowViewModel() 
+        { 
+
+        }
+
+        public void TextChanging()
+        {
+
+        }
+    }
+}
