4523523815099339dc22e34ae5f9ed4eb0c101e9
anastasiaoreskina <anastasiaoreskina@CCF1>
2023-06-16 00:59:55 +0300

    Заменила wrap panel на grid и добавила в дизайн диаграмму

--- SonClounds/ViewModel/FirstViewModel.cs
+++ SonClounds/ViewModel/FirstViewModel.cs
@@ -4,6 +4,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using LiveCharts;
+using LiveCharts.Wpf;
+
 
 namespace SonClounds.ViewModel
 {
@@ -89,5 +92,6 @@ namespace SonClounds.ViewModel
                 OnPropertyChenged();
             }
         }
+        
     }
 }
